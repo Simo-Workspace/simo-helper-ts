6537e91c6718f9c9c124613fefa0409d4c615df0
yUnreal <unrealwebdev@gmail.com>
2023-07-13 13:29:09 -0300

    fix(intents): using the correct intents now
    Using the correct intents to simo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { load } from 'env-smart';
 import { Client } from './core/models/Client';
+import { GatewayIntentBits } from 'discord.js';
 import { loadEvents } from './core/tools/load-events';
 import { loadCommands } from './core/tools/load-commands';
 
 export const client: Client<boolean> = new Client<boolean>({
-    intents: 0
+    intents: [
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
 loadEvents();

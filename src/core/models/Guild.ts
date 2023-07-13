6226fe1ea4ff130a5abbf6726d3c5586d82509ff
yUnreal <unrealwebdev@gmail.com>
2023-07-13 11:13:06 -0300

    feat(tags): implementing tags feature
    Implementing tags to guilds

--- src/core/models/Guild.ts
+++ src/core/models/Guild.ts
@@ -1,7 +1,14 @@
+import { Snowflake } from 'discord.js';
 import { Schema, model } from 'mongoose';
+import { GuildSchemaStructure, TagStructure } from '../types/typings';
 
-const guildSchema = new Schema({
-    
+const guildSchema = new Schema<GuildSchemaStructure>({
+    _id: {
+        type: String, required: true, minlength: 16, maxlength: 21
+    },
+    tags: {
+        type: Map, of: Object, default: new Map<Snowflake, TagStructure>()
+    }
 });
 
 export const Guild = model('GuildSchema', guildSchema);
--- src/core/types/typings.d.ts
+++ src/core/types/typings.d.ts
@@ -1,6 +1,17 @@
 import { Snowflake } from 'discord.js';
 
-
 export interface GuildSchemaStructure {
     _id: Snowflake;
+    tags: Map<Snowflake, TagStructure>;
+}
+
+export interface TagStructure {
+    name: string;
+    content: string;
+    type: TagType;
+}
+
+export enum TagType {
+    Event,
+    Function
 }

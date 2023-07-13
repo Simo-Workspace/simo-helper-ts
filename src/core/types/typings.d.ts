88633340379f466733060673e60e7cfd44f8ba16
yUnreal <unrealwebdev@gmail.com>
2023-07-13 13:20:59 -0300

    feat(types): adding and fixing types
    Fixing some types and adding news

--- src/core/types/typings.d.ts
+++ src/core/types/typings.d.ts
@@ -1,9 +1,11 @@
+import { Document } from 'mongoose';
 import { Client } from '../models/Client';
-import { Snowflake, Client as DiscordClient, ChatInputCommandInteraction, Message, ClientEvents, Awaitable } from 'discord.js';
+import { Snowflake, Client as DiscordClient, ChatInputCommandInteraction, Message, ClientEvents, Awaitable, AutocompleteInteraction, Collection } from 'discord.js';
 
 export interface GuildSchemaStructure {
     _id: Snowflake;
     tags: Map<Snowflake, TagStructure>;
+    prefix: string;
 }
 
 export interface TagStructure {
@@ -18,17 +20,19 @@ export enum TagType {
 }
 
 export interface OverrideClient extends DiscordClient {
-    commands: Map<RegExp, ClientCommand>;
-    applicationCommands: Map<string, ClientApplicationCommand>;
+    commands: Collection<string, ClientCommand>;
+    applicationCommands: Collection<string, ClientApplicationCommand>;
 }
 
 export interface ClientCommand {
-    name: RegExp;
+    name: string;
+    aliases: (never | string)[];
     run: (payload: CommandPayload<false>) => Promise<unknown>;
 }
 
 export interface ClientApplicationCommand {
     name: string;
+    autocomplete?: (interaction: AutocompleteInteraction) => Promise<unknown>;
     run: (payload: CommandPayload<true>) => Promise<unknown>;
 }
 
@@ -36,6 +40,10 @@ export interface CommandPayload<A extends boolean> {
     client: Client;
     interaction: A extends true ? ChatInputCommandInteraction : null;
     message: A extends true ? null : Message;
+    guildSchema: Document<unknown, Record<never, never>, GuildSchemaStructure> & Omit<GuildSchemaStructure & Required<{
+        _id: string;
+    }>, never>;
+    args: (A extends true ? never : string)[];
 }
 
 export interface ClientEvent<N extends keyof ClientEvents> {

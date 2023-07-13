9f2078bbe57c8227cf4f873cc96eac3063f16b3f
yUnreal <unrealwebdev@gmail.com>
2023-07-13 11:28:20 -0300

    feat(types): adding new types
    Adding types to client commands

--- src/core/types/typings.d.ts
+++ src/core/types/typings.d.ts
@@ -1,4 +1,5 @@
-import { Snowflake } from 'discord.js';
+import { Client } from '../models/Client';
+import { Snowflake, Client as DiscordClient, ChatInputCommandInteraction, Message } from 'discord.js';
 
 export interface GuildSchemaStructure {
     _id: Snowflake;
@@ -14,4 +15,25 @@ export interface TagStructure {
 export enum TagType {
     Event,
     Function
+}
+
+export interface OverrideClient extends DiscordClient {
+    commands: Map<RegExp, ClientCommand>;
+    applicationCommands: Map<string, ClientApplicationCommand>;
+}
+
+export interface ClientCommand {
+    name: RegExp;
+    run: (payload: CommandPayload<false>) => Promise<unknown>;
+}
+
+export interface ClientApplicationCommand {
+    name: string;
+    run: (payload: CommandPayload<true>) => Promise<unknown>;
+}
+
+export interface CommandPayload<A extends boolean> {
+    client: Client;
+    interaction: A extends true ? ChatInputCommandInteraction : null;
+    message: A extends true ? null : Message;
 }

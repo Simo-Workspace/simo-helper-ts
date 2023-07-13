443fdc9982335b22ee387c57dedf8afbb59673a5
yUnreal <unrealwebdev@gmail.com>
2023-07-13 12:02:17 -0300

    feat(raw-app-commands): adding raw application commands
    Adding raw application commands to client app commands

---
+++ src/core/tools/raw-app-commands.ts
@@ -0,0 +1,9 @@
+import { ApplicationCommandData, ApplicationCommandType, ApplicationCommandOptionType } from 'discord.js';
+
+export const RawAppCommands: ApplicationCommandData[] = [
+    {
+        name: 'fuck',
+        type: ApplicationCommandType.ChatInput,
+        description: 'Fuck out'
+    }
+];

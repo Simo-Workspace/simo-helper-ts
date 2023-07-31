5b4f7284200eecf0095ff6512ff3adb0d36a3c72
yUnreal <unrealwebdev@gmail.com>
2023-07-31 15:34:57 -0300

    remove(fuck.ts): removing unnecessary command

--- src/commands/app-commands/fuck.ts
+++
@@ -1,8 +0,0 @@
-import { ClientApplicationCommand } from '../../core/types/typings';
-
-export default {
-    name: 'fuck',
-    async run({ interaction }): Promise<void> {
-        await interaction.reply({ content: 'Fuck done', ephemeral: true });
-    }
-} as ClientApplicationCommand;
